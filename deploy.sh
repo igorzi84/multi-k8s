b5032780ebed32573b9a2fdc6a0d53ff82695607
Igor <igorzi84@gmail.com>
2019-01-24 20:31:14 -0500

    Added deployment script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t igorz/multi-client:latest -t igorz/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t igorz/multi-server:latest -t igorz/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t igorz/multi-worker:latest -t igorz/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push igorz/multi-client:latest
+docker push igorz/multi-server:latest
+docker push igorz/multi-worker:latest
+
+docker push igorz/multi-client:$SHA
+docker push igorz/multi-server:$SHA
+docker push igorz/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image depoloyements/server-deployment server=igorz/multi-server:$SHA
+kubectl set image depoloyements/client-deployment server=igorz/multi-client:$SHA
+kubectl set image depoloyements/worker-deployment server=igorz/multi-worker:$SHA
